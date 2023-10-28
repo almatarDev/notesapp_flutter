f97338b50db458a31dda2cb07dc6c9492eab179e
almatarDev <mygithub2000@gmail.com>
2023-10-28 03:43:46 +0200

    new editing

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,6 @@ late SharedPreferences sharedPref;
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   sharedPref = await SharedPreferences.getInstance();
-  //await DioHelper.init();
   runApp(const MyApp());
 }
 
@@ -22,14 +21,11 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Notes App',
       initialRoute: "login",
-      //initialRoute: sharedPref.getString("id") == null ? "login" : "home",
       routes: {
         "login": (context) => const LoginScreen(),
         "signup": (context) => const SignUpScreen(),
         "home": (context) => const HomeScreen(),
         "addnote": (context) => const AddNote(),
-        // "home": (context) => const HomeScreen(),
-        // "home": (context) => const HomeScreen(),
       },
     );
   }
